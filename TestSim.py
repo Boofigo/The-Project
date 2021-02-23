33472ecaafbe72a1c529d592eaddffc95554f165
Boofigo <ptrckcruz@gmail.com>
2021-02-23 13:36:04 -0800

    Presentation

--- TestSim.py
+++ TestSim.py
@@ -142,7 +142,7 @@ def main():
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
     s.addChannel(s.FLOODING_CHANNEL);
-    #s.addChannel(s.NEIGHBOR_CHANNEL);
+    s.addChannel(s.NEIGHBOR_CHANNEL);
 
 
     s.runTime(20);
@@ -150,7 +150,7 @@ def main():
     s.runTime(10);
     s.ping(1, 6, "Hello, World");
     s.runTime(40);
-    #s.neighborDMP(18);
+    s.neighborDMP(9);
     #s.findNeighbors(1, "Neighbor Found");
     s.runTime(10);
 
