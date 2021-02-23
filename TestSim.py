b9daa5a69b5bc0d40a19ef135c8982265f47221c
Boofigo <ptrckcruz@gmail.com>
2021-02-22 16:37:25 -0800

    Presenting

--- TestSim.py
+++ TestSim.py
@@ -149,9 +149,10 @@ def main():
     s.ping(1, 2, "Hello, World");
     s.runTime(10);
     s.ping(1, 6, "Hello, World");
+    s.runTime(40);
     s.neighborDMP(5);
     #s.findNeighbors(1, "Neighbor Found");
-    s.runTime(40);
+    s.runTime(10);
 
     
 if __name__ == '__main__':
