3e82051320e1b01fa382b4443824956dd675dd7e
Boofigo <ptrckcruz@gmail.com>
2021-03-01 22:44:50 -0800

    test

--- TestSim.py
+++ TestSim.py
@@ -149,8 +149,8 @@ def main():
     #s.ping(1, 2, "Hello, World");
     #s.runTime(10);
     s.ping(1, 6, "Hello, World");
-    #s.runTime(40);
-    #s.routeDMP(1);
+    s.runTime(10);
+    s.routeDMP(1);
     #s.neighborDMP(9);
     #s.findNeighbors(1, "Neighbor Found");
     s.runTime(10);
