d92ddafccdef58c1f77cb4576b9f7145fa1ebd05
Boofigo <ptrckcruz@gmail.com>
2021-03-12 21:36:41 -0800

    fix

--- includes/TCP_packet.h
+++ includes/TCP_packet.h
@@ -1,32 +1,28 @@
 #ifndef __TCP_packet_H__
 #define __TCP_packet_H__
  
-enum {
-	TCP_SYN = 0,
-	TCP_ACK = 1,
-	TCP_SYNACK = 2,
-	TCP_FIN = 3,
-	TCP_FINACK = 4,
-	TCP_DATA = 5,
-	TCP_DATAACK = 6
+#define Data_Flag 0
+#define Data_Ack_Flag 1
+#define SYN_Flag 2
+#define SYN_Ack_Flag 3
+#define Fin_Flag 4
+#define Fin_Ack_Flag 5
+#define Ack_Flag 6
+
+enum{
+	
+	TCP_PACKET_MAX_PAYLOAD_SIZE = 20
 };
 
-//PAYLOAD SIZES
-enum {
-	TCP_HEADER_SIZE = 8,
-	TCP_MAX_PAYLOAD_SIZE = 28 - TCP_HEADER_SIZE	//28 OR 20??
-};
-
-//TCP Packet
-typedef nx_struct tcpPack{
-	nx_uint8_t srcPort;
-	nx_uint8_t destPort;
-	nx_uint16_t seq;
-	nx_uint8_t ack;
-	nx_uint8_t flag;
-	nx_uint16_t adWindow;
-        nx_uint8_t payload[TCP_MAX_PAYLOAD_SIZE];
-}tcpPack;
+typedef nx_struct TCPpack{
+nx_uint8_t destport;
+nx_uint8_t srcport;
+nx_uint8_t flag;
+nx_uint8_t ACK;
+nx_uint8_t seq;
+nx_uint8_t Awindow;
+nx_uint16_t payload[TCP_PACKET_MAX_PAYLOAD_SIZE];
+}TCPpack;
 
 
 #endif
