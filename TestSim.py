54744ea7ebbf77581914791dde08e1fcea38661b
Boofigo <ptrckcruz@gmail.com>
2021-04-25 20:08:20 -0700

    Unicast

--- TestSim.py
+++ TestSim.py
@@ -187,7 +187,7 @@ def main():
     s.cmdConnect(3, 1, "Alice");
     s.runTime(30);
     s.cmdBroadcast(4, "Hello");
-    s.runTime(30);
+    s.runTime(60);
     s.cmdUnicast(4, 3, "Recieved");
     s.runTime(100);
 
